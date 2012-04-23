3a397e92a27daa5ba964e74a6c7f6bf49e2c31e5
zackzhu@gmail.com <zackzhu@gmail.com@c585d6ad-39fb-5653-dda1-89d3a70401a3>
2012-04-23 15:28:01 +0000

    added copyright blocks

--- src/com/zazhu/BlueHub/FrameParser.java
+++ src/com/zazhu/BlueHub/FrameParser.java
@@ -19,6 +19,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 /*
+Contributions:
+-Added packet checksum capabilities
+Zack Zhu, Wearable Computing Laboratory, ETH Zurich
+*/
+
+/*
  * FrameParser.java
  *
  */
